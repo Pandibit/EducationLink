82fa87ffb4b8ed6713c6b9002f6e2943fd86d3b1
Pandibit <109728836+Pandibit@users.noreply.github.com>
2024-04-26 18:30:09 +0200

    we

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,16 @@
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+#!/bin/bash
+
+# Installing Pipenv if not already installed
+python3.9 -m pip install pipenv
+
+# Installing dependencies from Pipfile
+echo "Installing dependencies using Pipenv..."
+pipenv install --deploy
+
+# Running collectstatic using Pipenv to ensure it uses the right environment
+echo "Collecting static files..."
+pipenv run python manage.py collectstatic --noinput
+
+# Optionally, you can add a command to check Django's availability
+echo "Checking Django availability..."
+pipenv run python -c "import django; print(django.get_version())"
