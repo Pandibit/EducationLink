f4875dc2c3152acf2d5270145959e4db8d4d9b86
Pandibit <109728836+Pandibit@users.noreply.github.com>
2024-04-26 17:56:36 +0200

    qw

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,2 @@
-source /Users/Pandi/.virtualenvs/EducationLink-o7_DB70E/Scripts/activate
-pip install -r requirements.txt 
-
+python -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
